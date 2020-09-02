2d3de05cf9575749ef17fc7dec69b2c539c18da3
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 12:22:16 +0200

    Init mongodb connection

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const config = require('./config');
+const connectDB = require('./server/config/db');
 
-// Config
-// Load env vars if env is not production
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: './server/local.env' });
-}
-const PORT = process.env.PORT || 5000;
+// Connect to mongodb instance
+connectDB();
 
 // Init express app
 const app = express();
 
+// Routes
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started and listening on port ${PORT}`);
+// Start server and listen for connections
+const server = app.listen(config.PORT, () => {
+  console.log(
+    `Server is running in ${config.NODE_ENV} mode and is listening on port ${config.PORT}...`,
+  );
+});
+
+// Error handling - close server
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
