19122d7030b4c147261a720ade05033b7bbefa52
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 15:54:32 +0200

    Created user model

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const config = require('../../config');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  chipsAmount: {
+    type: Number,
+    default: config.INITIAL_CHIPS_AMOUNT,
+  },
+  type: {
+    type: Number,
+    default: 0,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
