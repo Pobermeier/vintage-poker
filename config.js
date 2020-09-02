1fdc04e2978af2b9a27d71c4d05622ef9fb16b16
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 11:53:21 +0200

    Updated global config file

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
+const dotenv = require('dotenv');
+
+// Load env vars if env is not production
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './server/config/local.env' });
+}
+
 module.exports = {
   PORT: process.env.PORT || 5000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  MONGO_URI: process.env.MONGO_URI,
 };
