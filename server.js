c338dee7d53c052c26904247c32645b0f967fc30
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-02 11:23:31 +0200

    Added importing of env vars

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
-const app = express();
-
+// Config
+// Load env vars if env is not production
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './server/local.env' });
+}
 const PORT = process.env.PORT || 5000;
+
+// Init express app
+const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
