46455249b46ad6a7df66bf2f42f7a132027dab32
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-03 19:11:14 +0200

    Add config vars for SMTP relay & transactional mails

--- config.js
+++ config.js
@@ -12,4 +12,10 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV,
   INITIAL_CHIPS_AMOUNT: 30000,
   JWT_TOKEN_EXPIRES_IN: 3600000 * 24,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PW: process.env.SMTP_PW,
+  FROM_NAME: 'Vintage Poker Info',
+  FROM_EMAIL: 'no-reply@vintagepoker.app',
 };
