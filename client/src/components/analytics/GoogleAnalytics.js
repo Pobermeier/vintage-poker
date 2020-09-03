ab804eb496688588e1fd5495b053c483c7a0cd9c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-03 22:54:07 +0200

    Added GoogleAnalytics component

---
+++ client/src/components/analytics/GoogleAnalytics.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+const GoogleAnalytics = ({ location, history }) => {
+  useEffect(() => {
+    const gtag = window.gtag;
+
+    if (history.action === 'PUSH' && typeof gtag === 'function') {
+      gtag('config', '', {
+        page_title: document.title,
+        page_location: window.location.href,
+        page_path: location.pathname,
+      });
+    }
+  }, [location, history]);
+
+  return null;
+};
+
+export default withRouter(GoogleAnalytics);
