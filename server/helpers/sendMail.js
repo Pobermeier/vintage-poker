63716fa703f54d08032ca9f4f81ef6a569721775
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-03 19:13:08 +0200

    Created helper function to send transactional mails via node-mailer

---
+++ server/helpers/sendMail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+const config = require('../../config');
+
+const sendEmail = async (sendToEmail, template) => {
+  const { subject, text, html } = template;
+
+  const transporter = nodemailer.createTransport({
+    host: config.SMTP_HOST,
+    port: config.SMTP_PORT,
+    auth: {
+      user: config.SMTP_USER,
+      pass: config.SMTP_PW,
+    },
+  });
+
+  const message = {
+    from: `${config.FROM_NAME} <${config.FROM_EMAIL}>`,
+    to: sendToEmail,
+    subject,
+    text,
+    html,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
