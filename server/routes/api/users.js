7355f6cb64de6bf9fb8f8c6604479fae6877f85c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-03 13:16:39 +0200

    Implemented registration process

---
+++ server/routes/api/users.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../../../config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters',
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user =
+        (await User.findOne({ email })) || (await User.findOne({ name }));
+
+      if (user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Invalid credentials',
+            },
+          ],
+        });
+      }
+
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.JWT_SECRET,
+        { expiresIn: config.JWT_TOKEN_EXPIRES_IN },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        },
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Internal server error' });
+    }
+  },
+);
+
+module.exports = router;
