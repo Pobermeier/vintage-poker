89b4ce810082cb0a8742450e56b6daaa5b0d80db
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-03 11:05:28 +0200

    Created custom express-middleware to check jwt-tokens

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+
+const validateToken = (req, res, next) => {
+  const token = req.header('x-auth-token');
+
+  if (!token) return res.status(401).json({ msg: 'Unauthorized request!' });
+
+  try {
+    jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ msg: 'Unauthorized request!' });
+        console.error(err);
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('Internal auth error - error in token validation middleware');
+    res.status(500).json({ msg: 'Internal auth error' });
+  }
+};
+
+module.exports = validateToken;
