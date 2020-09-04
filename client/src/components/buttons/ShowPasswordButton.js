1444e62ed05a25982ad302ce25a40bff3eb7a6af
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-04 10:16:15 +0200

    Created encapsulated show password button

---
+++ client/src/components/buttons/ShowPasswordButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import EyeIcon from '../icons/EyeIcon';
+import styled from 'styled-components';
+
+const StyledShowPasswordButton = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 3px;
+  cursor: pointer;
+
+  svg {
+    width: 30px;
+  }
+`;
+
+const clickHandler = (ref) => {
+  if (ref.current.type === 'password') {
+    ref.current.type = 'text';
+  } else {
+    ref.current.type = 'password';
+  }
+};
+
+const ShowPasswordButton = ({ passwordRef }) => {
+  return (
+    <StyledShowPasswordButton onClick={() => clickHandler(passwordRef)}>
+      <EyeIcon />
+    </StyledShowPasswordButton>
+  );
+};
+
+ShowPasswordButton.propTypes = {
+  clickHandler: PropTypes.func,
+};
+
+export default ShowPasswordButton;
