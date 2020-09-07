f7249ec88c3be3f221fec7e148e9e0a2504f8e83
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-08 00:12:27 +0200

    Created custom authentication hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,97 @@
+import { useEffect, useState, useContext } from 'react';
+import Axios from 'axios';
+import setAuthToken from '../helpers/setAuthToken';
+import globalContext from '../context/global/globalContext';
+
+const useAuth = () => {
+  localStorage.token && setAuthToken(localStorage.token);
+
+  const { setIsLoading, setUserName, setEmail, setChipsAmount } = useContext(
+    globalContext,
+  );
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.token;
+
+    token && loadUser(token);
+
+    // eslint-disable-next-line
+  }, []);
+
+  const register = async (name, email, password) => {
+    setIsLoading(true);
+    try {
+      const res = await Axios.post('/api/users', {
+        name,
+        email,
+        password,
+      });
+
+      const token = res.data.token;
+
+      if (token) {
+        localStorage.setItem('token', token);
+        setAuthToken(token);
+        await loadUser(token);
+      }
+    } catch (error) {
+      alert(error);
+    }
+    setIsLoading(false);
+  };
+
+  const login = async (emailAddress, password) => {
+    setIsLoading(true);
+    try {
+      const res = await Axios.post('/api/auth', {
+        email: emailAddress,
+        password,
+      });
+
+      const token = res.data.token;
+
+      if (token) {
+        localStorage.setItem('token', token);
+        setAuthToken(token);
+        await loadUser(token);
+      }
+    } catch (error) {
+      alert(error);
+    }
+    setIsLoading(false);
+  };
+
+  const loadUser = async (token) => {
+    try {
+      const res = await Axios.get('/api/auth', {
+        headers: {
+          'x-auth-token': token,
+        },
+      });
+
+      const { name, email, chipsAmount } = res.data;
+
+      setIsLoggedIn(true);
+      setUserName(name);
+      setEmail(email);
+      setChipsAmount(chipsAmount);
+    } catch (error) {
+      localStorage.removeItem(token);
+      alert(error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    setUserName(null);
+    setEmail(null);
+    setChipsAmount(null);
+  };
+
+  return [isLoggedIn, login, logout, register];
+};
+
+export default useAuth;
