cf8443942558423e348190f1addf440454614020
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-09 13:40:27 +0200

    Init socketio

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const config = require('./config');
 const connectDB = require('./config/db');
 const configureMiddleware = require('./middleware');
 const configureRoutes = require('./routes');
+const socketio = require('socket.io');
+const gameSocket = require('./socket/index');
 
 // Connect and get reference to mongodb instance
 let db;
@@ -30,6 +32,11 @@ const server = app.listen(config.PORT, () => {
     `Server is running in ${config.NODE_ENV} mode and is listening on port ${config.PORT}...`,
   );
 });
+
+//  Handle real-time poker game logic with socket.io
+const io = socketio(server);
+
+io.on('connection', (socket) => gameSocket.init(socket, io));
 
 // Error handling - close server
 process.on('unhandledRejection', (err) => {
