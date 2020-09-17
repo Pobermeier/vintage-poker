297aa15ce9efe54503578057ca4ff8b6d3f88dbd
Patrick O <patrick.obermeier@outlook.com>
2020-09-17 11:20:59 +0200

    Added global config-file for client

---
+++ client/src/clientConfig.js
@@ -0,0 +1,7 @@
+const config = {
+  contentfulSpaceId: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  contentfulAccessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+  googleAnalyticsTrackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID,
+};
+
+export default config;
