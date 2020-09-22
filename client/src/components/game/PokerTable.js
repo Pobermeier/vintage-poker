138eac6072207ffaa5b6155ffb73f7dd3fe5ece6
Patrick O <patrick.obermeier@outlook.com>
2020-09-22 22:36:03 +0200

    Created styled PokerTable component

---
+++ client/src/components/game/PokerTable.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import table from '../../assets/game/table.svg';
+
+const StyledPokerTable = styled.img`
+  display: block;
+  pointer-events: none;
+  width: 95%;
+  margin: auto;
+
+  /* @media screen and (orientation: portrait) {
+    transform: rotate(90deg) scale(1.7);
+  } */
+`;
+
+const PokerTable = () => {
+  return <StyledPokerTable src={table} alt="Poker Table" />;
+};
+
+export default PokerTable;
