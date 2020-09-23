4210b03a7c12e8539f6b26bc621f1c2c3dc8c00a
Patrick O <patrick.obermeier@outlook.com>
2020-09-23 17:56:10 +0200

    Created reusable pill component

---
+++ client/src/components/game/InfoPill.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const InfoPill = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  padding: 0.5rem 2rem;
+  text-align: center;
+  background: #282215;
+  margin-bottom: 0.5rem;
+  opacity: 0.75;
+  color: #fffefc;
+  border-radius: 40px;
+`;
