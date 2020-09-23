cbf01316fd1b90d847ff962aef5293f16c0ee136
Patrick O <patrick.obermeier@outlook.com>
2020-09-23 11:13:13 +0200

    Created WrapperComponent for table information

---
+++ client/src/components/game/TableInfoWrapper.js
@@ -0,0 +1,12 @@
+import Text from '../typography/Text';
+import styled from 'styled-components';
+
+export const TableInfoWrapper = styled.div`
+  background-color: hsla(49, 63%, 92%, 60%);
+  border-radius: 2rem;
+  padding: 1rem 2rem;
+
+  ${Text} {
+    margin-bottom: 0;
+  }
+`;
