768e534b8a2ffef0a19323078ca9aa091a8839cd
Patrick O <patrick.obermeier@outlook.com>
2020-09-24 20:46:36 +0200

    Created imports-file for all poker card img-assets

---
+++ client/src/components/game/cards.js
@@ -0,0 +1,109 @@
+import c2 from '../../assets/game/cards/c2.png';
+import c3 from '../../assets/game/cards/c3.png';
+import c4 from '../../assets/game/cards/c4.png';
+import c5 from '../../assets/game/cards/c5.png';
+import c6 from '../../assets/game/cards/c6.png';
+import c7 from '../../assets/game/cards/c7.png';
+import c8 from '../../assets/game/cards/c8.png';
+import c9 from '../../assets/game/cards/c9.png';
+import c10 from '../../assets/game/cards/c10.png';
+import cJ from '../../assets/game/cards/cJ.png';
+import cQ from '../../assets/game/cards/cQ.png';
+import cK from '../../assets/game/cards/cK.png';
+import cA from '../../assets/game/cards/cA.png';
+import d2 from '../../assets/game/cards/d2.png';
+import d3 from '../../assets/game/cards/d3.png';
+import d4 from '../../assets/game/cards/d4.png';
+import d5 from '../../assets/game/cards/d5.png';
+import d6 from '../../assets/game/cards/d6.png';
+import d7 from '../../assets/game/cards/d7.png';
+import d8 from '../../assets/game/cards/d8.png';
+import d9 from '../../assets/game/cards/d9.png';
+import d10 from '../../assets/game/cards/d10.png';
+import dJ from '../../assets/game/cards/dJ.png';
+import dQ from '../../assets/game/cards/dQ.png';
+import dK from '../../assets/game/cards/dK.png';
+import dA from '../../assets/game/cards/dA.png';
+import h2 from '../../assets/game/cards/h2.png';
+import h3 from '../../assets/game/cards/h3.png';
+import h4 from '../../assets/game/cards/h4.png';
+import h5 from '../../assets/game/cards/h5.png';
+import h6 from '../../assets/game/cards/h6.png';
+import h7 from '../../assets/game/cards/h7.png';
+import h8 from '../../assets/game/cards/h8.png';
+import h9 from '../../assets/game/cards/h9.png';
+import h10 from '../../assets/game/cards/h10.png';
+import hJ from '../../assets/game/cards/hJ.png';
+import hQ from '../../assets/game/cards/hQ.png';
+import hK from '../../assets/game/cards/hK.png';
+import hA from '../../assets/game/cards/hA.png';
+import s2 from '../../assets/game/cards/s2.png';
+import s3 from '../../assets/game/cards/s3.png';
+import s4 from '../../assets/game/cards/s4.png';
+import s5 from '../../assets/game/cards/s5.png';
+import s6 from '../../assets/game/cards/s6.png';
+import s7 from '../../assets/game/cards/s7.png';
+import s8 from '../../assets/game/cards/s8.png';
+import s9 from '../../assets/game/cards/s9.png';
+import s10 from '../../assets/game/cards/s10.png';
+import sJ from '../../assets/game/cards/sJ.png';
+import sQ from '../../assets/game/cards/sQ.png';
+import sK from '../../assets/game/cards/sK.png';
+import sA from '../../assets/game/cards/sA.png';
+import hiddenhidden from '../../assets/game/cards/hiddenhidden.png';
+
+export default {
+  hiddenhidden,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  cJ,
+  cQ,
+  cK,
+  cA,
+  d2,
+  d3,
+  d4,
+  d5,
+  d6,
+  d7,
+  d8,
+  d9,
+  d10,
+  dJ,
+  dQ,
+  dK,
+  dA,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  h7,
+  h8,
+  h9,
+  h10,
+  hJ,
+  hQ,
+  hK,
+  hA,
+  s2,
+  s3,
+  s4,
+  s5,
+  s6,
+  s7,
+  s8,
+  s9,
+  s10,
+  sJ,
+  sQ,
+  sK,
+  sA,
+};
