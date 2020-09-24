767e9c91344ce1f96b13d4d6caeab54486ad8798
Patrick O <patrick.obermeier@outlook.com>
2020-09-24 22:22:05 +0200

    Created file to import seat user-img assets

---
+++ client/src/components/game/userImages.js
@@ -0,0 +1,6 @@
+import jackImg from '../../assets/img/jack-rounded-img@2x.png';
+import kingImg from '../../assets/img/king-rounded-img@2x.png';
+import queenImg from '../../assets/img/queen-rounded-img@2x.png';
+import queen2Img from '../../assets/img/queen2-rounded-img@2x.png';
+
+export default [jackImg, queen2Img, kingImg, jackImg, queenImg];
