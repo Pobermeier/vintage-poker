b245d8280a4e1b00165ed59ace01bd4b6971f862
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-28 20:54:56 +0200

    Added multiple breakpoints at which the UI is rescaled

--- client/src/components/game/PositionedUISlot.js
+++ client/src/components/game/PositionedUISlot.js
@@ -15,6 +15,22 @@ export const PositionedUISlot = styled.div`
   transform-origin: ${({ origin }) => origin || 'top left'};
 
   @media screen and (max-width: 868px) {
+    transform: ${({ scale }) => `scale(${+scale + 0.2})` || '1'};
+  }
+
+  @media screen and (max-width: 768px) {
+    transform: ${({ scale }) => `scale(${+scale + 0.15})` || '1'};
+  }
+
+  @media screen and (max-width: 668px) {
+    transform: ${({ scale }) => `scale(${+scale + 0.1})` || '1'};
+  }
+
+  @media screen and (max-width: 648px) {
+    transform: ${({ scale }) => `scale(${scale + 0.05})` || '1'};
+  }
+
+  @media screen and (max-width: 568px) {
     transform: ${({ scale }) => `scale(${scale})` || '1'};
   }
 `;
