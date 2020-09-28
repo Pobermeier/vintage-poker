911a93990e8f51807b74e20d7836d5548cda41dc
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-28 15:06:12 +0200

    Added greyscale filter to bg-image

--- client/src/components/decoration/TiledBackgroundImage.js
+++ client/src/components/decoration/TiledBackgroundImage.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import bgImage from '../../assets/img/bg.jpg';
 import styled from 'styled-components';
 
-export const TiledBackgroundImage = styled.div`
+const StyledTiledBackgroundImage = styled.div`
   display: none;
 
   @media screen and (min-width: 1024px) {
@@ -11,10 +12,32 @@ export const TiledBackgroundImage = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+    opacity: 0.5;
     background-image: url(${bgImage});
     background-repeat: repeat;
     background-size: 600px;
-    mix-blend-mode: darken;
-    filter: blur(3px);
   }
 `;
+
+const Wrapper = styled.div`
+  display: none;
+
+  @media screen and (min-width: 1024px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-color: rgba(36, 80, 105, 0.4);
+    filter: blur(1px);
+  }
+`;
+
+export const TiledBackgroundImage = () => (
+  <Wrapper>
+    <StyledTiledBackgroundImage />
+  </Wrapper>
+);
