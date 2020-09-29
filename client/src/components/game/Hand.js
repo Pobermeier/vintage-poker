917b7238926dd993b0ad35ed44a6892b95659752
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-29 14:42:12 +0200

    Minor styling update

--- client/src/components/game/Hand.js
+++ client/src/components/game/Hand.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Hand = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
 
   * ~ * {
     margin-left: -1.25rem;
