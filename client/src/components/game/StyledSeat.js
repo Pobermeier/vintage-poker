0adaa865b2d9a67ce11584111a02b31f6509c809
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-09-29 14:46:13 +0200

    Refactored styled component into separate file

---
+++ client/src/components/game/StyledSeat.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledSeat = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
