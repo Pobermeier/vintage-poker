0d8b71ed17321c448519b70bfff7c49409d6abd8
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-10-02 18:42:49 +0200

    Add GoogleAnalytics component for proper route-tracking

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Routes from './components/routing/Routes';
 import contentContext from './context/content/contentContext';
 import Text from './components/typography/Text';
 import modalContext from './context/modal/modalContext';
+import config from './clientConfig';
+import GoogleAnalytics from './components/analytics/GoogleAnalytics';
 
 const App = () => {
   const { isLoading, chipsAmount, setChipsAmount, setIsLoading } = useContext(
@@ -72,6 +74,7 @@ const App = () => {
           <Routes />
         </MainLayout>
       )}
+      {config.isProduction && <GoogleAnalytics />}
     </>
   );
 };
--- client/src/clientConfig.js
+++ client/src/clientConfig.js
@@ -1,4 +1,5 @@
 const config = {
+  isProduction: process.env.NODE_ENV === 'production',
   contentfulSpaceId: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
   contentfulAccessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
   googleAnalyticsTrackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID,
